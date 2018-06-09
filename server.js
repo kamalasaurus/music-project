d7b28e9b16a528df86552329ee00204f47215ba3
kamalasaurus <kamalasaurus@gmail.com>
2018-06-09 18:57:59 -0400

    switched to static-server from live-server to not have crossed sockets

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const {copy, concat, compile} = require('./exportify.js');
 
-const EventEmitter = require('events');
-
-const liveServer = require('live-server');
+const StaticServer = require('static-server');
 
 const Twit = require('twit');
-const secrets = JSON.parse(require('./secrets.json'));
+const secrets = require('./secrets.json');
 
 //build upon launch //////////////////////////////////////
 
@@ -23,18 +21,57 @@ compile(scripts);
 const T = new Twit(secrets);
 //const stream = T.stream('statuses/filter', {track: 'love'});
 
-const stream = T.stream('statuses/sample');
+//let stream = T.stream('statuses/sample');
+
+let stream = { on: function() {} };
 
 stream.on('tweet', (tweet) => {
   console.log(tweet);
+  // emit event with tweet as data
 });
+
+stream.on('limit', (limitMessage) => {
+  console.log(limitMessage);
+});
+
+stream.on('disconnect', (disconnectMessage) => {
+  console.log(disconnectMessage)
+  rerunConnect = true;
+  attemptReconnect();
+});
+
+stream.on('connected', (response) => {
+  rerunConnect = false;
+  exponentialDropoff = 1000;
+});
+
+let rerunConnect = false;
+let exponentialDropoff = 1000;
+
+function attemptReconnect() {
+  if (rerunConnect) {
+    setTimeout(() => {
+      stream = T.stream('statuses/sample');
+      exponentialDropoff += exponentialDropoff;
+      attemptReconnect();
+    }, exponentialDropoff);
+  }
+}
 
 //setup websockets //////////////////////////////////////
 
+// listen to tweet event, fire websocket event
 
 
 
-liveServer.start({
-  root: 'app'
+//start server //////////////////////////////////////////
+
+const server = new StaticServer({
+  rootPath: './app',
+  port: 1337
+});
+
+server.start(() => {
+  console.log('server listening to', server.port);
 });
 
